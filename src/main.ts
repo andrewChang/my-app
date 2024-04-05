1a69c34114139772a734524fe290b7ad4f99096b
zhangyue <zhangyue@hw.com>
2024-04-06 01:14:52 +0800

    solve the issue of breakpoint

--- src/main.ts
+++ src/main.ts
@@ -15,14 +15,13 @@ const createWindow = () => {
       preload: path.join(__dirname, 'preload.js'),
     },
   });
-
   // and load the index.html of the app.
   if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
     mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
   } else {
     mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
   }
-
+  debugger;
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
 };
--- vite.main.config.ts
+++ vite.main.config.ts
@@ -17,6 +17,7 @@ export default defineConfig((env) => {
       rollupOptions: {
         external,
       },
+      sourcemap: true,
     },
     plugins: [pluginHotRestart('restart')],
     define,
